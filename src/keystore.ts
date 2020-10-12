cabcc2cef0ecf41ffe569e010a510a5811b411df
Keefer Taylor <keefer@keefertaylor.com>
2020-10-11 22:50:22 -0400

    signer abstraction

---
+++ src/keystore.ts
@@ -0,0 +1,14 @@
+/** 
+ * An object which can store keys for signing.
+ * 
+ * Note that this interface is implemented by `TezosKmsClient` in the `@tacoinfra/tezos-kms` library.
+ */
+export default interface KeyStore {
+  /**
+   * Sign the given bytes and return a base58check signature.
+   * 
+   * @param buffer The bytes to sign.
+   * @returns a Base58 signature.
+   */
+  signOperationBase58(buffer: Buffer): Promise<string>
+}
--- src/signer.ts
+++ src/signer.ts
@@ -1,21 +1,38 @@
 import { CodingUtils } from '@tacoinfra/tezos-core'
+import Parser from './parser'
+import KeyStore from './keystore'
 
 /** Signs payloads if they meet criteria. */
 export default class Signer {
+  /** 
+   * Construct a new Signer.
+   * 
+   * @param keyStore an object which will store keys to sign.
+   */
+  public constructor(private readonly keyStore: KeyStore) {
+  }
+
   /**
    * Sign the given payload if it passes filter functions.
    * 
    * @param payload The payload to sign, as hex.
    * @returns A base58check encoded signature.
    */
-  public sign(payload: string): string {
+  public async sign(payload: string): Promise<string> {
     console.log(`Received request to sign ${payload}`)
 
     if (CodingUtils.isHex(payload)) {
       throw new Error("Request was not hex.")
     }
-    const _bytes = CodingUtils.hexToBytes(payload)
+    const bytes = CodingUtils.hexToBytes(payload)
 
-    return 'edsig'
+    const operation = Parser.parse(bytes)
+
+    // TODO(keefertaylor): Acquire Lock
+    // TODO(keefertaylor): Verify filters are passed - is baking operation
+    // TODO(keefertaylor): Is High water mark
+
+    const signature = await this.keyStore.signOperationBase58(Buffer.from(bytes))
+    return signature
   }
 }
